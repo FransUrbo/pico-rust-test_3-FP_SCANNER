e57b9aafcc98b99b0d3f2e808a387cfc26a5aa13
Turbo Fredriksson <turbo@bayour.com>
2024-05-03 17:34:33 +0100

    Use my own GIT versions for R503 and WS2812.
    * Latest version of the R503 module require that the IRQ binding
      is done here, not in the module.

--- src/set_colour.rs
+++ src/set_colour.rs
@@ -4,18 +4,20 @@
 use defmt::{debug, info, error};
 
 use embassy_executor::Spawner;
-use embassy_rp::peripherals::PIO1;
-use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::peripherals::{PIO1, UART0};
+use embassy_rp::uart::InterruptHandler as UARTInterruptHandler;
+use embassy_rp::pio::{InterruptHandler as PIOInterruptHandler, Pio};
 use embassy_rp::bind_interrupts;
 use embassy_time::Timer;
 
 use {defmt_rtt as _, panic_probe as _};
 
-use r503::r503::R503;
-use r503::ws2812::Ws2812 as Ws2812;
+use ws2812;
+use r503;
 
 bind_interrupts!(pub struct Irqs {
-    PIO1_IRQ_0 => InterruptHandler<PIO1>;	// NeoPixel
+    PIO1_IRQ_0 => PIOInterruptHandler<PIO1>;	// NeoPixel
+    UART0_IRQ  => UARTInterruptHandler<UART0>;	// Fingerprint scanner
 });
 
 // ================================================================================
@@ -27,11 +29,11 @@ async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
     // Initialize the fingerprint scanner
-    let mut r503 = R503::new(p.UART0, p.PIN_16, p.DMA_CH0, p.PIN_17, p.DMA_CH1, p.PIN_13.into());
+    let mut r503 = r503::R503::new(p.UART0, Irqs, p.PIN_16, p.DMA_CH0, p.PIN_17, p.DMA_CH1, p.PIN_13.into());
 
     // Initialize the multi-colour LED.
     let Pio { mut common, sm0, .. } = Pio::new(p.PIO1, Irqs);
-    let mut ws2812 = Ws2812::new(&mut common, sm0, p.DMA_CH3, p.PIN_15);
+    let mut ws2812 = ws2812::Ws2812::new(&mut common, sm0, p.DMA_CH3, p.PIN_15);
 
     debug!("NeoPixel OFF");
     ws2812.write(&[(0,0,0).into()]).await;
